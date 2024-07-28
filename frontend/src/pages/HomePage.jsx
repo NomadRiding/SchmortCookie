033d8a85e83c53d00ee47303dbe635df4ba22330
nomadriding <fundora.a@gmail.com>
2024-07-27 22:53:17 -0400

    framework, navbar, and css

---
+++ frontend/src/pages/HomePage.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import { Link } from "react-router-dom";
+import '../components/Homepage.css';
+
+function HomePage() {
+  return (
+    <div>HomePage</div>
+  )
+}
+
+export default HomePage
